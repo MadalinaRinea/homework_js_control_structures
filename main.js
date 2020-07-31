8e9795649372c42c0a4483d2b61252cfcb071f59
MadalinaRinea <66742630+MadalinaRinea@users.noreply.github.com>
2020-08-01 01:24:23 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 // display in the console the numbers from 1 to 20
+console.log('1. The numbers from 1 to 20');
 
 for(let i = 1; i <= 20; i++) {
     console.log(i);
 }
 
 // display in the console the odd numbers from 1 to 20
-
+console.log('2. The odd numbers from 1 to 20');
 for(let i = 1; i <= 20; i++) {
     if (i % 2 != 0) {
         console.log(i);
@@ -13,7 +14,7 @@ for(let i = 1; i <= 20; i++) {
 }
 
 // compute the sum of the elements of an array and display it in the console
- 
+console.log('3. The sum of the elements of an array');
 function sum(arr) {
     let sum = 0;
     
@@ -28,7 +29,7 @@ let myArr = [15, -5, 5, 0, 100, 2277, 13.3, 5, 45, 5];
 console.log('Sum = ' + sum(myArr));
 
 // compute the maximum of the elements of an array and display it in the console 
-
+console.log('4. The maximum of the elements of an array');
 function max(arr) {
     let max = arr[0];
 
@@ -44,7 +45,7 @@ function max(arr) {
 console.log('Maximum = ' + max(myArr));
 
 // compute how many times a certain element appears in an array
-
+console.log('5. How many times a certain element appears in an array');
 function smth(x, arr) {
     let contor = 0;
     
@@ -58,14 +59,13 @@ function smth(x, arr) {
 }
 
 
-console.log(smth(5, myArr));
-
+console.log('How many times, 5,  appears in myArr: ' + smth(5, myArr));
 let anArr = [4, 'g', 67, 'k', 'l' ,'g', 579, 'g', 4568, 'g', 546, 'g']
-console.log(smth('g', anArr));
+console.log('How many times, g, appears in anArr: ' + smth('g', anArr));
 
 
 // Challenge
-
+console.log('6. Challenge');
 function challenge() {
     var matrix = [];
     for(let i = 0; i < 4; i++) {
